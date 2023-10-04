7eaa51c2550f083849340f78877c7062fb92ec59
pardoelizabeth <112141700+pardoelizabeth@users.noreply.github.com>
2023-10-04 17:36:09 -0600

    Update inicio.blade.php

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -1,4 +1,3 @@
-
 @extends('layouts.header')
 
 @section('description')
@@ -18,47 +17,70 @@
         </button>
         <div class="collapse navbar-collapse justify-content-center" id="navbarNavDropdown">
             <ul class="navbar-nav">
-                <li class="nav-item dropdown mr-3">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown">Alumno</a>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" href="#">Trámite A</a>
-                        <a class="dropdown-item" href="#">Trámite B</a>
-                    </div>
-                </li>
-                <li class="nav-item dropdown mr-3">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown">Profesor</a>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" href="#">Trámite C</a>
-                        <a class="dropdown-item" href="#">Trámite D</a>
-                    </div>
-                </li>
-                <li class="nav-item dropdown mr-3">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown">Tutor</a>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" href="#">Trámite E</a>
-                        <a class="dropdown-item" href="#">Trámite F</a>
-                    </div>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown">Staff</a>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" href="#">Trámite G</a>
-                        <a class="dropdown-item" href="#">Trámite H</a>
-                    </div>
-                </li>
+                <li class="nav-item mr-5">
+                    <a class="nav-link btn btn-primary rounded" href="/hctc/inicio">Formatos para solicitudes</a>
+                </li> 
+                <li class="nav-item mr-5">
+                    <a class="nav-link btn btn-primary rounded" href="#">Seguimiento de solicitudes</a>
+                </li> 
             </ul>
         </div>
     </nav>
 
-
-
-
     <!-- FontAwesome (para el ícono del botón) -->
-    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css">
+
     <!-- Bootstrap JS (optional) -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
-@endsection
+    <style>
+        .btn.btn-danger.btn-sm {
+            background-color: #004a98;
+            color: white;
+            border: none;
+        }
 
+        .btn.btn-danger.btn-sm:hover {
+            background-color: white;
+            color: #004a98;
+        }
+
+        .btn.btn-primary.rounded {
+            background-color: white;
+            color: #004a98;
+            border: none;
+        }
+
+        .btn-primary.rounded:hover {
+            background-color: #004a98 !important;
+            color: white !important;
+        }
+
+        /* Estilo para los botones grandes en línea */
+        .btn-large-container {
+            display: flex;
+            justify-content: center;
+            margin-top: 40px; /* Ajusta este valor según tus preferencias de separación */
+        }
+
+        .btn-large {
+            width: 25%; /* Ancho de los botones */
+            height: 120px; /* Altura de los botones */
+            font-size: 20px; /* Tamaño de fuente de los botones */
+            margin: 20px 30px; /* Espacio entre los botones */
+            text-align: center; /* Centra el texto horizontalmente */
+            display: flex;
+            align-items: center; /* Centra el texto verticalmente */
+            justify-content: center;
+        }
+    </style>
+    
+    <!-- Botones grandes en línea -->
+    <div class="btn-large-container">
+        <a class="btn btn-large btn-primary" href="#">Materia Única</a>
+        <a class="btn btn-large btn-primary" href="#">Carga Máxima</a>
+        <a class="btn btn-large btn-primary" href="/hctc/solicitudes/titulacion">Opciones de Titulación</a>
+    </div>
+@endsection
