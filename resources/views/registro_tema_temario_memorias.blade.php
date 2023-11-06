00e195a58946125313127537f6f4fc60066627a0
Alexis Flores <49456992+AlanFlores29@users.noreply.github.com>
2023-11-06 01:58:26 -0600

    Vista registro tema y temario

--- resources/views/registro_tema_temario_memorias.blade.php
+++ resources/views/registro_tema_temario_memorias.blade.php
@@ -1,18 +1,54 @@
 @extends('layouts.header')
 
 @section('content')
-
+<div class="modal fade" id="ModalAgregar" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-4" id="exampleModalLabel" data-bs-toggle="modal" data-bs-target="#ModalAgregar">Agregar Capitulo</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+      <form action="" method="post">
+        @csrf
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Orden</label>
+    <input type="text" class="form-control" id="txtrpe" aria-describedby="emailHelp" name="txtorden">
+    
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Titulo</label>
+    <input type="text" class="form-control" id="txtrpe" aria-describedby="emailHelp" name="txttitulo">
+    
+  </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+        <button type="submit" class="btn btn-primary">Guardar</button>
+      </div>
+    </form>
+    </div>  
+    </div>
+  </div>
+</div>
 {{-- @include('usuario_cinta') --}}
 
 <div class="container">
-        <h1>Registro de Tema y Temario Memorias de Actividad Profesional</h1>
-
+        <h1>Memorias de Actividad Profesional</h1>
+         <h1>Registro Tema y Temario</h1>
         <br>
 
                 <div class="row">
                     <div class="col-md-10">
                         <div class="form-group d-flex">
-                            <label for="semestre" class="mr-2" style="">Domicilio </label>
+                            <label for="semestre" class="mr-2" style="">Generacion:</label>
+                            <input type="text" id="promedio" class="form-control" value="2019" readonly >
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-10">
+                        <div class="form-group d-flex">
+                            <label for="semestre" class="mr-2" style="">Direccion:</label>
                             <input type="text" id="promedio" class="form-control">
                         </div>
                     </div>
@@ -20,73 +56,142 @@
                 <div class="row">
                     <div class="col-md-10">
                         <div class="form-group d-flex">
-                            <label for="semestre" class="mr-2" style="">Teléfono </label>
+                            <label for="semestre" class="mr-2" style="">Telefono:</label>
                             <input type="text" id="promedio" class="form-control">
                         </div>
                     </div>
                 </div>
-                <div class="row">
+<nav>
+  <div class="nav nav-tabs" id="nav-tab" role="tablist">
+    <button class="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Asesor y Coasesor</button>
+    <button class="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-tema" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Tema y Temario</button>
+    <button class="nav-link" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-empresa" type="button" role="tab" aria-controls="nav-contact" aria-selected="false">Datos de la Empresa</button>
+  </div>
+</nav>
+<div class="tab-content" id="nav-tabContent">
+    <br>
+  <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+     <div class="col-md-8">
+       <div class="form-group d-flex">
+                <label for="asesor" class="mr-2" style="">Asesor:</label>
+                <input type="text" id="Asesor" class="form-control" placeholder="Nombre Asesor">
+        </div>
+       </div>
+       <div class="col-md-8">
+       <div class="form-group d-flex">
+                <label for="coasesor" class="mr-2" style="">Coasesor:</label>
+                <input type="text" id="Coasesor" class="form-control" placeholder="Nombre CoAsesor">
+        </div>
+       </div>
+   </div>
+<div class="tab-pane fade" id="nav-tema" role="tabpanel" aria-labelledby="nav-profile-tab">
+     <div class="col-md-10">
+       <div class="form-group d-flex">
+                <label for="tema" class="mr-2" style="">Tema:</label>
+                <textarea class="form-control" ></textarea>
+        </div>
+      </div>
+<div class="p-5 table-responsive">
+<div class="row justify-content-between">
+<div class="col-4">
+    <label for="temario" class="mr-2" style="">Temario</label>
+    </div>
+    <div class="col-4">
+    <a class="btn btn-outline-primary" href="" data-bs-toggle="modal" data-bs-target="#ModalAgregar">Agregar Capitulo</a>
+    </div>
+</div>
+<table  class="table table-striped table-hover table-bordered">
+  <thead class="bg-primary text-white">
+    <tr>
+      <th scope="col">Orden</th>
+      <th scope="col">Titulo</th>
+      <th scope="col"></th>
+      
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Capitulo 1: Introduccion</td>
+      <td>
+      <a href="" data-bs-toggle="modal" data-bs-target="#ModalEditar"class="btn btn-info"><i class="fa-solid fa-pen-to-square"></i></a>
+      <a href=""  class="btn btn-danger"> <i class="fa-solid fa-trash"></i></a>
+        </td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Capitulo 2: Descripcion</td>
+      <td>
+            <a href="" data-bs-toggle="modal" data-bs-target="#ModalEditar"class="btn btn-info"> <i class="fa-solid fa-pen-to-square"></i></a>
+            <a href=""  class="btn btn-danger"> <i class="fa-solid fa-trash"></i></a>
+       </td>
+       
+    </tr>
+    
+  </tbody>
+</table>
+</div>
+
+</div>
+   <div class="tab-pane fade" id="nav-empresa" role="tabpanel" aria-labelledby="nav-contact-tab">
+        <div  class="container-fluid">
+           <div class="row">
+               <div class="col-md-10">
+                   <div class="form-floating mb-3">
+                   
+                   <input type="text" id="nombremp" class="form-control" placeholder="Nombre de la Empresa">
+                   <label for="nombremp" class="mr-2" style="">Nombre de la Empresa:</label>
+                </div>
+               </div>
+            </div>
+            <div class="row">
+                   <div class="col-md-10">
+                       <div class="form-floating mb-3">
+                           <input type="text" id="direccion" class="form-control" placeholder="Direccion">
+                           <label for="direccion">Direccion:</label>
+                       </div>
+                    </div>
+            </div> 
+            <div class="row">
                     <div class="col-md-10">
-                        <div class="form-group d-flex">
-                            <label for="semestre" class="mr-2" style="">Asesor Propuesto</label>
-                            <input type="text" id="promedio" class="form-control">
-                        </div>
+                            <div class="form-floating mb-3">
+                              <input type="text" id="telefono" class="form-control" placeholder="Telefono de la empresa">
+                              <label for="telefono" >Telefono(s):</label>
+                            </div>
                     </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-10">
-                        <div class="form-group d-flex">
-                            <label for="semestre" class="mr-2" style="">Tema Propuesto </label>
-                            <input type="text" id="promedio" class="form-control">
-                        </div>
+            </div>
+            <div class="row">
+                    <div class="col-md-5">
+                            <div class="form-floating mb-3">
+                               <input type="text" id="extension" class="form-control" placeholder="Extension">
+                               <label for="extension" >EXT:</label>
+                             </div>
+
                     </div>
-                </div>
-
-                <div class="row">
-                <div class="col-md-10">
-        <div class="form-group d-flex">
-            <label for="temario" class="mr-2" style="">Temario Propuesto</label>
-        </div>
+            </div>
+            <div class="row">
+                        <div class="col-md-10">
+                           <div class="form-floating mb-3">
+                                <input type="text" id="ciudad" class="form-control" placeholder="Ciudad de la empresa ">
+                                <label for="ciudad" class="mr-2" style="">Ciudad:</label>
+                            </div>
+                        </div>
+            </div>
+         </div>
     </div>
 </div>
 
-<div class="col-md-10">
-    <div class="form-group d-flex">
-        <label for="temario" class="mr-2" style="">Tema</label>
-        <input type="text" id="temario" class="form-control">
-        <button id="agregar-tema" class="btn btn-primary ml-2 agregar-btn">+</button>
-    </div>
-</div>
-
-<div class="row">
-    <div class="col-md-10">
-        <div class="form-group d-flex">
-            <label for="semestre" class="mr-2" style=""></label>
-            <ul id="temas-ingresados"></ul>
-        </div>
-    </div>
-</div>
-
-<div class="row">
-    <div class="col-md-10">
-        <div class="form-group d-flex">
-            <label for="semestre" class="mr-2" style=""></label>
-            <button id="editar-tema" class="btn btn-primary ml-2" style="display: none;">Editar Tema</button>
-            <button id="eliminar-tema" class="btn btn-danger ml-2" style="display: none;">Eliminar Tema</button>
-        </div>
-    </div>
-</div>
-
-                <br>
-
-                <div class="form-group">
+<br>
+<div class="form-group">
                     <button class="btn btn-primary mr-2" id="registrar-solicitud">Registrar Solicitud</button>
-                    <form id="formulario" method="GET" action="{{ route('materiaUnicaPdf.show') }}" target="_blank">
+                    <form id="formulario" method="GET" action="" target="_blank">
                     <button class="btn btn-success" id="descargar-formato">Descargar Formato</button>
                     </form>
-                </div>
 </div>
-
+</div>
+<script src="https://kit.fontawesome.com/5298f3cc9e.js" crossorigin="anonymous"></script>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 <style>
     .container {
         margin-top: 35px;
@@ -131,7 +236,7 @@
 </style>
 
 
-<<script>
+<script>
     $(document).ready(function() {
         var temas = [];
         var temaSeleccionado = null;
